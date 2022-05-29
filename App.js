10be6a7839866101dac9b677a8d2d40677d17a14
Mohammed Sadik <msadikjowel@gmail.com>
2022-05-29 18:19:28 +0600

    home page done

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import { typography } from './src/theme/typography';
+import { colors } from './src/theme/colors';
+import { spacing } from './src/theme/spacing';
+import { NavigationContainer, DarkTheme } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/screens/Home';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  // font
+  const [loaded] = useFonts({
+    "Antonio-Medium": require('./assets/fonts/Antonio-Medium.ttf'),
+    "Spartan-Bold": require('./assets/fonts/Spartan-Bold.ttf'),
+    "Spartan-Regular": require('./assets/fonts/Spartan-Regular.ttf'),
+  });
+
+  if (!loaded) {
+    return <Text>Font is loading...</Text>;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Home" component={Home} />
+          {/* <Stack.Screen name="Details" component={Details} /> */}
+        </Stack.Navigator>
+      </NavigationContainer>
+      <StatusBar style='light' />
+    </>
   );
 }
 
---
+++ src/screens/Home.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import { View, Text, SafeAreaView, StyleSheet, Image, Pressable } from 'react-native';
+import { colors } from '../theme/colors';
+import { spacing } from '../theme/spacing';
+import { typography } from '../theme/typography';
+
+const Home = () => {
+    return (
+        <SafeAreaView style={styles.container} >
+            <View style={styles.logoArea}>
+                <View style={styles.logoImage}>
+                    <Image
+                        source={require('./../../assets/images/logo.png')}
+                    />
+                </View>
+            </View>
+            <View style={styles.textArea}>
+                <View style={styles.textImage}>
+                    <Image
+                        source={require('./../../assets/images/rectangle.png')}
+                    />
+                </View>
+                <Text style={styles.textHeadline}>Non-Contact {"\n"} Deliveries</Text>
+                <Text style={styles.textDetails}>When placing an order, select the option "Contactless delivery" and the courier will leave your order at the door.</Text>
+
+                <Pressable style={styles.button}>
+                    <Text style={styles.buttonText}>ORDER NOW</Text>
+                </Pressable>
+
+                <Text style={styles.dismissText}>Dismiss</Text>
+            </View>
+        </SafeAreaView>
+    );
+}
+
+export default Home;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: colors.violet,
+    },
+    logoArea: {
+        height: '40%',
+    },
+    logoImage: {
+        marginTop: 70,
+        marginLeft: 35,
+        backgroundColor: colors.greenSoft,
+        padding: spacing[8],
+        width: 90,
+        borderRadius: 50
+    },
+    textArea: {
+        backgroundColor: colors.whiteAsh,
+        height: '60%',
+        borderTopLeftRadius: 50,
+        borderTopRightRadius: 50,
+        padding: spacing[10],
+        alignItems: 'center',
+        // justifyContent: 'center',
+    },
+    textImage: {
+        backgroundColor: colors.white,
+        padding: spacing[8],
+        width: 100,
+        borderRadius: 50
+    },
+    textHeadline: {
+        textAlign: 'center',
+        marginVertical: spacing[5],
+        fontSize: spacing[8],
+        fontFamily: typography.PrimaryBold,
+        fontWeight: 'bold',
+        color: colors.navy,
+    },
+    textDetails: {
+        textAlign: 'center',
+        color: colors.navy,
+        fontSize: spacing[4],
+        marginBottom: spacing[10],
+        lineHeight: spacing[5],
+    },
+    button: {
+        backgroundColor: colors.softGreen,
+        width: "100%",
+        height: 60,
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+
+    },
+    buttonText: {
+        textAlign: 'center',
+        color: colors.white,
+        fontWeight: 'bold',
+        fontSize: spacing[4],
+    },
+    dismissText: {
+        color: colors.navy,
+        fontSize: spacing[4],
+        marginTop: spacing[8],
+        textTransform: 'uppercase'
+    }
+})
+
